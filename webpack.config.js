f930cca18d847555c135eb4291e2c8e858498172
AdalZanabria <adal.zanabria@gmail.com>
2022-01-19 23:19:54 -0600

    Agregados los loaders y plugins para html y css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -18,6 +20,18 @@ module.exports = {
                     loader: "babel-loader",
                 },
             },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: "body",
+            template: "./public/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin(),
+    ],
 };
